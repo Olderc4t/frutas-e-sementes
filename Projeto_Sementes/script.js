299e6a5d55113ac32b51850186ab163fe23bb94c
Lucas Magalhães <103212436+Olderc4t@users.noreply.github.com>
2023-03-03 17:16:01 -0300

    0.1v
    primeira versão para ja irmos testando backend

--- Projeto_Sementes/script.js
+++ Projeto_Sementes/script.js
@@ -7,12 +7,12 @@
   function toggleSidebar() {
     showSidebar = !showSidebar;
     if (showSidebar) {
-      navigationHeader.style.marginLeft = '-10vw'
+      navigationHeader.style.marginLeft = '-100vw'
       navigationHeader.style.animationName = 'showSidebar'
       content.style.filter = 'blur(2px)'
     }
     else {
-      navigationHeader.style.marginLeft = '-100vw'
+      navigationHeader.style.marginLeft = '-10vw'
       navigationHeader.style.animationName = '';
       content.style.filter = '';
     }
